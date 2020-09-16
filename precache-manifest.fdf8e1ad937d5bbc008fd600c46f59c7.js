055dcd94b0ab0e86075d03b2f82b75448fb34bf2
Moshe Kolodny <kolodny@github.com>
2020-09-15 23:04:24 -0400

    test commit

---
+++ precache-manifest.fdf8e1ad937d5bbc008fd600c46f59c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8c10a2f44e09fc42732faab02fda83",
+    "url": "/spa-redirect-demo/index.html"
+  },
+  {
+    "revision": "6dcd789ecd9d3ee30d10",
+    "url": "/spa-redirect-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "417648b11e2227743c85",
+    "url": "/spa-redirect-demo/static/js/2.c634b359.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spa-redirect-demo/static/js/2.c634b359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcd789ecd9d3ee30d10",
+    "url": "/spa-redirect-demo/static/js/main.b1c6980a.chunk.js"
+  },
+  {
+    "revision": "4605bd10d99ea2920a1a",
+    "url": "/spa-redirect-demo/static/js/runtime-main.82c5f7cb.js"
+  }
+]);
